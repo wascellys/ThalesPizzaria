9c700ebb5b3c1cb806ac7e9a230308c246a168b0
Edy Wascellys <edywascellys@gmail.com>
2016-01-13 15:34:06 -0200

    add arquivos

---
+++ Pizzaria/WebContent/resources/js/closeDialogIFSucess.js
@@ -0,0 +1,8 @@
+function closeDialogIfSucess(xhr, status, args, dialogWidget, dialogId) {
+    if (args.validationFailed || args.KEEP_DIALOG_OPENED) {
+        jQuery('#'+dialogId).effect("bounce", {times : 4, distance : 20}, 100);
+    } 
+    else {
+    dialogWidget.hide();
+    }
+}
